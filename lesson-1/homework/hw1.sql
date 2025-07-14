92f92e7b3374e8c0eb1e49eee94d61391c1cdf7e
rguzelbaev <44321121+rguzelbaev@users.noreply.github.com>
2025-07-14 20:54:57 +0500

    Update hw1.sql

--- lesson-1/homework/hw1.sql
+++ lesson-1/homework/hw1.sql
@@ -1,1 +1,20 @@
-Select "Hello world"
+Notes before doing the tasks:
+
+Tasks should be solved using SQL Server.
+Case insensitivity applies.
+Alias names do not affect the score.
+Scoring is based on the correct output.
+One correct solution is sufficient.
+Easy
+Define the following terms: data, database, relational database, and table.
+List five key features of SQL Server.
+What are the different authentication modes available when connecting to SQL Server? (Give at least 2)
+Medium
+Create a new database in SSMS named SchoolDB.
+Write and execute a query to create a table called Students with columns: StudentID (INT, PRIMARY KEY), Name (VARCHAR(50)), Age (INT).
+Describe the differences between SQL Server, SSMS, and SQL.
+Hard
+Research and explain the different SQL commands: DQL, DML, DDL, DCL, TCL with examples.
+Write a query to insert three records into the Students table.
+Restore AdventureWorksDW2022.bak file to your server. (write its steps to submit) You can find the database from this link :https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorksDW2022.bak
+SQL-
